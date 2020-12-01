ac145811cd6475fe1de9ec202833ee0de1422f4c
Jonathan Perry <jpper@umich.edu>
2020-12-01 12:07:00 +0900

    Added code for first question (understand solution)

---
+++ problems/1-pair-in-array-with-sum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+void printPairs(int arr[], int arr_size, int sum)
+{
+    unordered_set<int> s;
+    for (int i = 0; i < arr_size; i++)
+    {
+        int temp = sum - arr[i];
+
+        if (s.find(temp) != s.end())
+        {
+            cout << "There exists a pair adding to the given sum value";
+            return;
+        }
+        s.insert(arr[i]);
+    }
+}
+
+/* Driver Code */
+int main()
+{
+    int A[] = {1, 15, 4, 45, 6, 10, 8};
+    int n = 16;
+    int arr_size = sizeof(A) / sizeof(A[0]);
+
+    // Function calling
+    printPairs(A, arr_size, n);
+
+    return 0;
+}
